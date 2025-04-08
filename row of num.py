f64eaee3fbfd154865ccdeb3850e3612b4ead1cc
ayaan261-goat <ktm.ayaangupta@gmail.com>
2025-04-08 08:35:26 +0545

    hi

---
+++ row of num.py
@@ -0,0 +1,13 @@
+i = 1
+while i<=5:
+    j=1
+    while j<=10:
+        print(j,end="")
+        j=j+1
+    i=i+1
+    print()
+print()
+for i in range(1,5):
+    for j in range(1,11):
+        print(j, end='')
+    print()
